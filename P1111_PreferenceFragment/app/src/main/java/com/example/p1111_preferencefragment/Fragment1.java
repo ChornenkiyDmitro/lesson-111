cef866925c6bd8f042b400bc05509b8d37a45b0a
ChornenkiyDmitro <74198378+ChornenkiyDmitro@users.noreply.github.com>
2020-12-15 11:59:15 +0200

    lesson 111
    lesson 111

---
+++ P1111_PreferenceFragment/app/src/main/java/com/example/p1111_preferencefragment/Fragment1.java
@@ -0,0 +1,11 @@
+package com.example.p1111_preferencefragment;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class Fragment1 extends PreferenceFragment {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref1);
+    }
+}
